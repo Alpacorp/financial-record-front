f7a6dfc2df6aca166ea05f052803b3c50be7c9ba
Alpacorp <alejandro.palacios88@gmail.com>
2023-02-20 23:24:38 -0500

    Set call apis and base url

--- src/apis/billsApi.ts
+++ src/apis/billsApi.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-const baseUrl = process.env.REACT_APP_URL;
+let baseUrl: string | undefined = "";
+
+if (process.env.REACT_APP_ENVIRONMENT === "development") {
+  baseUrl = process.env.REACT_APP_URL_DEV || "http://localhost:4000";
+} else {
+  baseUrl = process.env.REACT_APP_URL_PROD;
+}
 
 const billsApi = axios.create({
   baseURL: baseUrl,
